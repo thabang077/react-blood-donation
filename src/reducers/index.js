8a4ef74c3b62db98442bcfdbf00d3508f2f01f1d
thabang077 <64046663+thabang077@users.noreply.github.com>
2025-11-26 01:40:58 +0200

    Update
    Removed commented-out code for DonationCandidates.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,6 @@
 import { combineReducers } from "redux";
-// import DonationCandidates from "./donateCandidate";
 import donatetionCandidateReducer from "./donateCandidate";
 
 export const reducers = combineReducers({
-  // Define your reducers here
-  // DonationCandidates,
   DonationCandidate: donatetionCandidateReducer,
 });
