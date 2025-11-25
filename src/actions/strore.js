6720c96a1901cb199d82ee1c5c6907b15ecbc6ba
thabang077 <64046663+thabang077@users.noreply.github.com>
2025-11-26 01:38:37 +0200

    Update
    Removed commented out import for donateCandidateReducer.

--- src/actions/strore.js
+++ src/actions/strore.js
@@ -1,7 +1,6 @@
 import { createStore, applyMiddleware, compose } from "@reduxjs/toolkit";
 import { thunk } from "redux-thunk";
 import { reducers } from "../reducers";
-// import donateCandidateReducer from "../reducers/index";
 
 export const store = createStore(
   reducers,
